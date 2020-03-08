6f041b61177f50729805579bb6c427b2e179f7c6
Manjunath <gmanjurdg@gmail.com>
2020-03-08 13:49:15 +0530

    done

---
+++ src/components/Calculator/CalculatorDisplay.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Calculator.css';
+import AutoScalingText from './AutoScaling';
+
+export default class CalculatorDisplay extends React.Component {
+    render() {
+        const { value, ...props } = this.props
+
+        const language = navigator.language || 'en-US'
+        let formattedValue = parseFloat(value).toLocaleString(language, {
+            useGrouping: true,
+            maximumFractionDigits: 6
+        })
+
+        // Add back missing .0 in e.g. 12.0
+        const match = value.match(/\.\d*?(0*)$/)
+
+        if (match)
+            formattedValue += (/[1-9]/).test(match[0]) ? match[1] : match[0]
+
+        return (
+            <div {...props} className="calculatorDisplay">
+                <AutoScalingText>{formattedValue}</AutoScalingText>
+            </div>
+        )
+    }
+}
---
+++ src/components/Calculator/CalculatorKey.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PointTarget from 'react-point'
+import './Calculator.css';
+// const PointTarget = ReactPoint.PointTarget
+
+export default class CalculatorKey extends React.Component {
+    render() {
+        const { onPress, className, ...props } = this.props
+
+        return (
+            <PointTarget onPoint={onPress}>
+                <button className={`calculator-key ${className}`} {...props} />
+            </PointTarget>
+        )
+    }
+}
